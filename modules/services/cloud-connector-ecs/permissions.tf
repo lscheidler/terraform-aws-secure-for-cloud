1d81b1a3cadaeac435c479f8638d41eb5fd23125
Lars Eric Scheidler <lars-eric.scheidler@ratepay.com>
2023-01-03 16:05:23 +0100

    test

--- modules/services/cloud-connector-ecs/permissions.tf
+++ modules/services/cloud-connector-ecs/permissions.tf
@@ -1,7 +1,7 @@
 locals {
-  ecs_task_role_id          = var.is_organizational ? data.aws_iam_role.task_inherited[0].id : aws_iam_role.task[0].id
-  ecs_task_role_arn         = var.is_organizational ? data.aws_iam_role.task_inherited[0].arn : aws_iam_role.task[0].arn
-  ecs_task_role_name_suffix = var.is_organizational ? var.organizational_config.connector_ecs_task_role_name : var.connector_ecs_task_role_name
+  ecs_task_role_id          = var.is_organizational && var.organizational_config.connector_ecs_task_role_name != null ? data.aws_iam_role.task_inherited[0].id : aws_iam_role.task[0].id
+  ecs_task_role_arn         = var.is_organizational && var.organizational_config.connector_ecs_task_role_name != null ? data.aws_iam_role.task_inherited[0].arn : aws_iam_role.task[0].arn
+  ecs_task_role_name_suffix = var.is_organizational && var.organizational_config.connector_ecs_task_role_name != null ? var.organizational_config.connector_ecs_task_role_name : var.connector_ecs_task_role_name
 }
 
 data "aws_ssm_parameter" "sysdig_secure_api_token" {
@@ -17,12 +17,12 @@ data "aws_ssm_parameter" "sysdig_secure_api_token" {
 # - duplicated in /examples/organizational/permissions.tf
 #---------------------------------
 data "aws_iam_role" "task_inherited" {
-  count = var.is_organizational ? 1 : 0
+  count = var.is_organizational && var.organizational_config.connector_ecs_task_role_name != null ? 1 : 0
   name  = var.organizational_config.connector_ecs_task_role_name
 }
 
 resource "aws_iam_role" "task" {
-  count              = var.is_organizational ? 0 : 1
+  count              = var.is_organizational && var.organizational_config.connector_ecs_task_role_name != null ? 0 : 1
   name               = "${var.name}-${local.ecs_task_role_name_suffix}"
   assume_role_policy = data.aws_iam_policy_document.task_assume_role[0].json
   path               = "/"
@@ -30,7 +30,7 @@ resource "aws_iam_role" "task" {
 }
 
 data "aws_iam_policy_document" "task_assume_role" {
-  count = var.is_organizational ? 0 : 1
+  count = var.is_organizational && var.organizational_config.connector_ecs_task_role_name != null ? 0 : 1
   statement {
     effect = "Allow"
     principals {
